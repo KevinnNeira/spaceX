1b46405cfc49eba853c271529036843bb628f729
KevinnNeira <160855637+KevinnNeira@users.noreply.github.com>
2024-06-27 07:46:54 -0500

    feat: :sparkles: Se añadio la funcion de la paginacion
    Tambien se les agrego 2 iconos al footer

---
+++ js/modules/pagination.js
@@ -0,0 +1,114 @@
+import { 
+    getAllCapsules
+} from "../components/capsules.js";
+import { 
+    nameRockets 
+} from "./titleRocket.js";
+import { getAllRockets,  } from "../components/rockets.js";
+
+export const load = async()=>{
+    let header__title = document.querySelector("#header__title");
+    header__title.innerHTML = `
+        <div class="load"></div>
+    `;
+}
+export const clear = async()=>{
+    let header__title = document.querySelector("#header__title");
+    header__title.innerHTML = ``;
+}
+const getRocketsId = async(e)=>{
+    e.preventDefault();
+    console.log(e.target);
+    let a = e.target.parentElement.children;
+    for(let val of a){
+        val.classList.remove('activo');
+    }
+    e.target.classList.add('activo');
+    
+    let Rocket = await getRocketsId(e.target.id);
+    await clear();
+    await nameRockets(Rocket.name) 
+}
+export const paginationRockets = async()=>{
+    let rockets = await getAllRockets();
+    let div = document.createElement("div");
+    div.classList.add("buttom__paginacion")
+  
+    rockets.forEach((val,id) => {
+        let a = document.createElement("a");
+        a.setAttribute("href","#");
+        a.id = val.id;
+        a.textContent = id+1;
+        a.addEventListener("click", getRocketsId)
+        div.appendChild(a);
+    });
+    
+    let [a1,a2,a3,a4] = div.children
+    a3.click();
+    // <div class="buttom__paginacion">
+    //     <a href="#">&laquo;</a> 
+    //     <a href="#" class="activo">1</a>
+    //     <a href="#">2</a>
+    //     <a href="#">3</a>
+    //     <a href="#">4</a>
+    //     <a href="#">&raquo;</a>
+    // </div>
+    
+    return div;
+}
+
+export const getCapsulesId = async(e)=>{
+    e.preventDefault();
+    if(e.target.dataset.page){
+        let paginacion = document.querySelector("#paginacion");
+        paginacion.innerHTML = ""
+        paginacion.append(await paginationCapsules(Number(e.target.dataset.page)))
+    }
+    let a = e.target.parentElement.children;
+    for(let val of a){
+        val.classList.remove('activo');
+    }
+    e.target.classList.add('activo');
+}
+export const paginationCapsules = async(page=1, limit=4)=>{  
+     
+    let {docs, pagingCounter, totalPages, nextPage} = await getAllCapsules(page, limit)
+
+    let div = document.createElement("div");
+    div.classList.add("buttom__paginacion")
+
+    
+    let start = document.createElement("a");
+    start.setAttribute("href","#");
+    start.innerHTML = "&laquo";
+    start.setAttribute("data-page", (page==1) ? totalPages : page-1)
+    start.addEventListener("click", getCapsulesId)
+    div.appendChild(start);
+    docs.forEach((val,id) => {
+        let a = document.createElement("a");
+        a.setAttribute("href","#");
+        a.id = val.id;
+        a.textContent = pagingCounter;
+        a.addEventListener("click", getCapsulesId)
+        div.appendChild(a);
+        pagingCounter++
+    });
+    let end = document.createElement("a");
+    end.setAttribute("href","#");
+    end.innerHTML = "&raquo;";
+    end.setAttribute("data-page", (page && nextPage) ? page+1 : 1)
+    end.addEventListener("click", getCapsulesId)
+    div.appendChild(end);
+    console.log(div);
+    let [back, a1,a2,a3,a4, next] = div.children
+    a1.click();
+    // <div class="buttom__paginacion">
+    //     <a href="#">&laquo;</a> 
+    //     <a href="#" class="activo">1</a>
+    //     <a href="#">2</a>
+    //     <a href="#">3</a>
+    //     <a href="#">4</a>
+    //     <a href="#">&raquo;</a>
+    // </div>
+    return div;
+ }
