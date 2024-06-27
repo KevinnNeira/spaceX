9a107f086b767f209ee7149ec83fedee4ef6bc44
KevinnNeira <160855637+KevinnNeira@users.noreply.github.com>
2024-06-27 14:39:43 -0500

    style: :lipstick: Se les agrego 2 gifs provicionales para la decoracion de la web
    Tambien se añadio la funcion para mostrar el serial de la capsula en el header__title

--- js/components/capsules.js
+++ js/components/capsules.js
@@ -16,3 +16,26 @@ export const getAllCapsules = async (page,limit)=>{
     console.log(data);
     return data;
 }
+export const getReuseCount  = async() =>{
+    let config = {
+        headers:{
+            "content-type": "application/json"
+        },
+        method: "POST",
+        body: JSON.stringify({
+            "options": {
+                "select": {
+                    "name": 1,
+                    "mass": 1
+                },
+                "sort":{
+                    "mass.kg": "desc"
+                },
+                "limit": 1
+            }
+        })
+    }
+    let res = await fetch("https://api.spacexdata.com/v4/capsules/query", config);
+    let {docs:[{mass} = maxMassRocket]} = await res.json();
+    return mass;
+}
--- js/modules/title.js
+++ js/modules/title.js
@@ -3,3 +3,8 @@ export const nameRockets = async(name)=>{
     header__title.innerHTML = "";
     header__title.textContent = name;
 }
+export const serialCapsule = async (serial) =>{
+    let header__title = document.querySelector("#header__title");
+    header__title.innerHTML = "";
+    header__title.textContent = serial;
+}
