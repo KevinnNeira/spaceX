0558e32337b5b54e2bc96505199e2b131bfb490b
KevinnNeira <160855637+KevinnNeira@users.noreply.github.com>
2024-06-28 08:47:12 -0500

    feat: :sparkles: Se les agregaron los encabezados a la seccion de capsules
    Se va a comenzar a insertar la informacion dentro de cada uno de ellos

--- js/modules/title.js
+++ js/modules/title.js
@@ -3,8 +3,13 @@ export const nameRockets = async(name)=>{
     header__title.innerHTML = "";
     header__title.textContent = name;
 }
-export const serialCapsule = async (serial) =>{
+export const serialCapsule = async (serial) => {
     let header__title = document.querySelector("#header__title");
-    header__title.innerHTML = "";
-    header__title.textContent = serial;
+
+    if (header__title) {
+        header__title.innerHTML = "";
+        header__title.textContent = serial;
+    } else {
+        console.error("Element with ID 'header__title' not found.");
+    }
 }
