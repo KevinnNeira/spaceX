ef355bd355073b9a7a21c1f34a31ff1922b3da2d
KevinnNeira <160855637+KevinnNeira@users.noreply.github.com>
2024-07-02 08:30:34 -0500

    feat: :sparkles: Se introdujo la funcion en todos los modulos para obtener tanto todos como unicamente por id
    Se va a acomenzar a hacer la obtencion de datos en la seccion de capsules

---
+++ js/components/landpads.js
@@ -0,0 +1,23 @@
+export const getAllLandpads = async (page,limit)=>{
+    let config = {
+        headers:{
+            "content-type": "application/json"
+        },
+        method: "POST",
+        body: JSON.stringify({
+            "options": {
+                page,
+                limit
+            }
+        })
+    }
+    let res = await fetch("https://api.spacexdata.com/v4/landpads/query", config)
+    let data = await res.json();
+    console.log(data);
+    return data;
+}
+export const getLandpadsId = async (id)=>{
+    let res = await fetch(`https://api.spacexdata.com/v4/landpads/${id}`)
+    let data = await res.json();
+    return data;
+}
---
+++ js/components/launches.js
@@ -0,0 +1,23 @@
+export const getAllLaunches = async (page,limit)=>{
+    let config = {
+        headers:{
+            "content-type": "application/json"
+        },
+        method: "POST",
+        body: JSON.stringify({
+            "options": {
+                page,
+                limit
+            }
+        })
+    }
+    let res = await fetch("https://api.spacexdata.com/v4/launches/query", config)
+    let data = await res.json();
+    console.log(data);
+    return data;
+}
+export const getLaunchesId = async (id)=>{
+    let res = await fetch(`https://api.spacexdata.com/v4/launches/${id}`)
+    let data = await res.json();
+    return data;
+}
